17f99fab0d4fbfa4f8a9ac3b4c3e7ec4013ae3c4
gadisom <jeongwon.kim@gmail.com>
2025-05-16 15:53:24 +0900

    Add: TCA dependency

---
+++ SwMaeStrong/SwMaeStrong/Tab/MainTabReducer.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabReducer.swift
+//  SwMaeStrong
+//
+//  Created by 김정원 on 5/16/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct MainTabReducer: Reducer {
+    
+}
